9e4f6bb5438fa5d82f23e967a9a11ef37c9d18c4
dpernes <diogo.pernes.cunha@gmail.com>
2020-01-02 16:00:43 +0000

    Added TrancosSeq dataset

--- datasets.py
+++ datasets.py
@@ -1,10 +1,12 @@
 import os
+import random
 
 import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.io
 import skimage.transform as SkT
+import torch
 from skimage import io
 from torch.utils.data import Dataset
 
@@ -28,7 +30,8 @@ class Trancos(Dataset):
         with open(os.path.join(self.path, 'images', 'cam_annotations.txt')) as f:
             for line in f:
                 img_f, cid = line.split()
-                self.cam_ids[img_f] = int(cid)
+                if img_f in self.image_files:
+                    self.cam_ids[img_f] = int(cid)
 
     def __len__(self):
         return len(self.image_files)
@@ -72,6 +75,70 @@ class Trancos(Dataset):
 
         return X, mask, density, count, cam_id
 
+class TrancosSeq(Trancos):
+    def __init__(self, train=True, path='./TRANCOS_v3', size_red=8, transform=None, gamma=1e3, max_len=None):
+        super(TrancosSeq, self).__init__(train=train, path=path, size_red=size_red, transform=transform, gamma=gamma)
+        self.img2idx = {img: idx for idx, img in enumerate(self.image_files)}
+
+        self.seqs = []
+        prev_cid = -1
+        with open(os.path.join(self.path, 'images', 'cam_annotations.txt')) as f:
+            for line in f:
+                img_f, cid = line.split()
+                if img_f in self.image_files:
+                    if cid == prev_cid:
+                        self.seqs[-1].append(img_f)
+                    else:
+                        self.seqs.append([img_f])
+                    prev_cid = cid
+
+        if max_len is None:
+            self.max_len = max([len(seq) for seq in self.seqs])
+        else:
+            self.max_len = max_len
+
+    def __len__(self):
+        return len(self.seqs)
+
+    def __getitem__(self, i):
+        seq = self.seqs[i]
+
+        if len(seq) > self.max_len:
+            start_idx = random.randint(0, len(seq)-self.max_len)
+            stop_idx = start_idx + self.max_len
+            seq_len = self.max_len
+        else:
+            start_idx = 0
+            stop_idx = len(seq)
+            seq_len = stop_idx
+
+        for j in range(start_idx, stop_idx):
+            idx = self.img2idx[seq[j]]
+            Xj, maskj, densityj, countj, cam_id = super().__getitem__(idx)
+
+            if j == start_idx:
+                X, mask, density = Xj.unsqueeze(0), maskj.unsqueeze(0), densityj.unsqueeze(0)
+                count = torch.Tensor([countj])
+            else:
+                X = torch.cat((X, Xj.unsqueeze(0)), dim=0)
+                mask = torch.cat((mask, maskj.unsqueeze(0)), dim=0)
+                density = torch.cat((density, densityj.unsqueeze(0)), dim=0)
+                count = torch.cat((count, torch.Tensor([countj])), dim=0)
+
+        if seq_len < self.max_len:
+            Xpad = torch.zeros((self.max_len-seq_len, *X.shape[1:]))
+            maskpad = torch.zeros((self.max_len-seq_len, *mask.shape[1:]))
+            densitypad = torch.zeros((self.max_len-seq_len, *density.shape[1:]))
+            countpad = torch.zeros(self.max_len-seq_len)
+
+            X = torch.cat((X, Xpad), dim=0)
+            mask = torch.cat((mask, maskpad), dim=0)
+            density = torch.cat((density, densitypad), dim=0)
+            count = torch.cat((count, countpad), dim=0)
+
+        return X, mask, density, count, cam_id, seq_len
+
+
 if __name__ == '__main__':
     data = Trancos(train=True, path='/ctm-hdd-pool01/DB/TRANCOS_v3', transform=NP_T.RandomHorizontalFlip(0.5))
 
@@ -93,3 +160,9 @@ if __name__ == '__main__':
         ax3.imshow(Xh.astype('uint8'))
         ax3.set_title('Highlighted vehicles')
         plt.show()
+
+
+    data = TrancosSeq(train=True, path='/ctm-hdd-pool01/DB/TRANCOS_v3', transform=NP_T.ToTensor())
+
+    for i, (X, mask, density, count, cid, seq_len) in enumerate(data):
+        print('Seq {}: cid={}, len={}'.format(i, cid, seq_len))
